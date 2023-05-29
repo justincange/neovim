2b94699e179480af8c4b60934266a466cb9bbe5c
Justin Cange <jc5525@columbia.edu>
2023-05-28 20:47:14 -0400

    files of config

---
+++ lua/plug.lua
@@ -0,0 +1,11 @@
+-- [[ plug.lua ]]
+vim.cmd [[set packpath^=~/.config/nvim/site/pack]]
+
+return require('packer').startup(function(use)
+  -- [[ Plugins Go Here ]]
+  use {                                              -- filesystem navigation
+    'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons'        -- filesystem icons
+  }
+end)
+
