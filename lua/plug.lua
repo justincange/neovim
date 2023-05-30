4a7a4bdc8e46b121ef44d61590b46abd270dd09b
Justin Cange <jc5525@columbia.edu>
2023-05-30 17:52:29 -0400

    optional -> not optional

--- lua/plug.lua
+++ lua/plug.lua
@@ -1,17 +1,13 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
--- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
-  -- Simple plugins can be specified as strings
   use 'rstacruz/vim-closer'
 
-  use 'nvim-tree/nvim-tree.lua'
-
+  use {'nvim-tree/nvim-tree.lua',
+  requires = {'nvim-tree/nvim-web-devicons'}}
   use { 'mhinz/vim-startify' }                       -- start screen
   use { 'DanilaMihailov/beacon.nvim' }               -- cursor jump
   use {'andymass/vim-matchup', event = 'VimEnter'}
@@ -30,33 +26,23 @@ return require('packer').startup(function(use)
     config = 'vim.cmd[[ALEEnable]]'
   }
 
-  -- Plugins can have dependencies on other plugins
   use {
     'haorenW1025/completion-nvim',
-    opt = true,
-    requires = {{'hrsh7th/vim-vsnip', opt = true}, {'hrsh7th/vim-vsnip-integ', opt = true}}
+    requires = {{'hrsh7th/vim-vsnip' }, {'hrsh7th/vim-vsnip-integ',  }}
   }
 
-
-  -- Plugins can have post-install/update hooks
   use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'}
   use 'morhetz/gruvbox'
   vim.cmd('colorscheme gruvbox')
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
-  -- Use specific branch, dependency and run lua file after load
    use {   
     'glepnir/galaxyline.nvim', branch = 'main', config = function() require'statusline' end,
     requires = {'nvim-tree/nvim-web-devicons'}
   }
 
-  -- Use dependency and run lua function after load
   use {
     'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
     config = function() require('gitsigns').setup() end
   }
-
-  -- You can specify multiple plugins in a single call
-
-  -- You can alias plugin names
 end)
